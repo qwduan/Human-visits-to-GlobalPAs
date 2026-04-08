2038281e14c549cf79494eca03fe19d8f247634d
Wendy <148213410+qwduan@users.noreply.github.com>
2026-04-08 18:27:48 +0100

    Update SR_visit.R

--- SR_visit.R
+++ SR_visit.R
@@ -2,10 +2,10 @@ library(ggnewscale); library(DHARMa); library(readr)
 library(dplyr); library(mgcViz)
 library(terra); library(sf)
 
-fra_df <- read_csv('xxx/output/visit/v4/home_fra_update.csv')
+fra_df <- read_csv('xxx/output/visit/home_fra_update.csv')
 home <- fra_df %>% select(lat, lon, pop)
 
-intensity_df <- read_csv('xxx/output/visit/v4/df_inten_per.csv') %>%
+intensity_df <- read_csv('xxx/output/visit/df_inten_per_adj.csv') %>%
   left_join(home, by = c('visit_latitude'='lat', 'visit_longitude' = 'lon'))
 
 sr_bird <- rast("xxx/Birds/Richness_10km.tif")
@@ -63,21 +63,17 @@ r_proj_corrected <- r_proj * zonal_factor
 global(r_wgs, fun = 'sum', na.rm = TRUE)
 global(r_proj, fun = 'sum', na.rm = TRUE)
 global(r_proj_corrected , fun = "sum", na.rm = TRUE)
-#4162254469/4063781131
-#8673311806
 
 
 r_final <- resample(r_proj_corrected, sr_bird, method = "sum")
 global(r_final , fun = "sum", na.rm = TRUE)
-#writeRaster(r_final, 'E:/data/reporj2.tif')
-#4063330597
+
 
 centroids <- as.points(r_final)
 bird <- terra::extract(sr_bird, centroids)
 mam <- terra::extract(sr_mam, centroids)
 amp <- terra::extract(sr_amp, centroids)
 visit <- terra::extract(r_final, centroids)
-
 
 
 centroids_sf <- st_as_sf(centroids)
@@ -100,7 +96,6 @@ centroids_df <- centroids_df[-idx_inside, ]
 centroids_df$visit_intensity[is.na(centroids_df$visit_intensity)] <- 0
 
 
-
 centroids_df <- centroids_df %>%
   mutate(sr = rowSums(across(c(sr_bird, sr_mam, sr_amp)), na.rm = TRUE))
 centroids_df <- centroids_df %>%
@@ -108,8 +103,7 @@ centroids_df <- centroids_df %>%
 
 
 com <- centroids_df
-
-over <- read_csv('xxx/output/visit/v4/factor10km_pa_intersect.csv')
+over <- read_csv('xxx/output/visit/factor10km_pa_intersect.csv')
 
 over <- over %>%
   mutate(id = as.character(Id))
@@ -129,26 +123,24 @@ com_overlap$in_PA_alt <- ifelse(com_overlap$overlap > 0, "Inside PA", "Outside P
 
 
 
-
-hf_proj <- rast('xxx/output/visit/v3/factor/hf.reproject.tif')
+hf_proj <- rast('xxx/output/visit/factor/hf.reproject.tif')
 hf_final <- resample(hf_proj, sr_bird, method = "average")
 
-slope_proj <- rast('xxx/output/visit/v3/factor/slope.reproject.tif')
+slope_proj <- rast('xxx/output/visit/factor/slope.reproject.tif')
 slope_final <- resample(slope_proj, sr_bird, method = "average")
 
-ele_proj <- rast('xxx/output/visit/v3/factor/ele.reproject.tif')
+ele_proj <- rast('xxx/output/visit/factor/ele.reproject.tif')
 ele_final <- resample(ele_proj, sr_bird, method = "average")
 
-tem_proj <- rast('xxx/output/visit/v3/factor/tem.reproject.tif')
+tem_proj <- rast('xxx/output/visit/factor/tem.reproject.tif')
 tem_final <- resample(tem_proj, sr_bird, method = "average")
 
-ndvi_proj <- rast('xxx/output/visit/v3/factor/ndvi.reproject.tif')
+ndvi_proj <- rast('xxx/output/visit/factor/ndvi.reproject.tif')
 ndvi_final <- resample(ndvi_proj, sr_bird, method = "average")
 
-land_proj <- rast('xxx/output/visit/v3/factor/land.reproject.tif')
+land_proj <- rast('xxx/output/visit/factor/land.reproject.tif')
 land_proj <- as.factor(land_proj)
 land_final <- resample(land_proj, sr_bird, method = "mode")
-
 
 
 pts <- vect(com_overlap, geom = c("lon", "lat"), crs = target_crs)
@@ -159,25 +151,18 @@ com_overlap$tem <- terra::extract(tem_final, pts)[,2]
 com_overlap$ndvi <- terra::extract(ndvi_final, pts)[,2]
 com_overlap$land <- terra::extract(land_final, pts)[,2]
 
-
-prec_orig <- rast("xxx/output/visit/v3/factor/prec.tif") 
-
+prec_orig <- rast("xxx/output/visit/factor/prec.tif") 
 prec_reproj <- terra::project(prec_orig, target_crs, method = "bilinear")
-
 zone_orig <- rasterize(adm0_vect, prec_orig, field = "COUNTRY")
 zone_proj <- rasterize(adm0_proj_vect, prec_reproj, field = "COUNTRY")
-
 sum_prec_orig <- zonal(prec_orig, zone_orig, fun = "sum", na.rm = TRUE)
 sum_prec_proj <- zonal(prec_reproj, zone_proj, fun = "sum", na.rm = TRUE)
-
 names(sum_prec_orig)[2] <- "prec_sum_orig"
 names(sum_prec_proj)[2] <- "prec_sum_proj"
-
 factor_df <- adm0_proj %>%
   left_join(sum_prec_orig, by = "COUNTRY") %>%
   left_join(sum_prec_proj, by = "COUNTRY") %>%
   mutate(prec_correction = prec_sum_orig / prec_sum_proj)
-
 
 factor_raster <- rasterize(vect(factor_df), prec_reproj, field = "prec_correction")
 prec_corrected <- prec_reproj * factor_raster
@@ -185,33 +170,25 @@ prec_corrected <- prec_reproj * factor_raster
 global(prec_orig, fun = 'sum', na.rm = TRUE)
 global(prec_reproj, fun = 'sum', na.rm = TRUE)
 global(prec_corrected, fun = "sum", na.rm = TRUE)
-#1649449806,4604927570,1586466152
 
 prec_fina <- resample(prec_corrected, sr_bird, method = "sum")
-
 com_overlap$prec <- terra::extract(prec_fina, pts)[,2]
-write.csv(com_overlap, 'xxx/output/visit/v4/gam_factor_v2.csv')
 
 
 
-#com <- com_overlap
-com <- read_csv('xxx/output/visit/v4/gam_factor_v2.csv')
-com$logsr <- log(com$sr)
-com$land <- as.factor(com$land)
-com$logv <- log(com$visit_intensity+0.1)
 
-
-hist(com$logsr)
-hist(com$logv)
-
+com <- com_overlap
 
 library(mgcv)
 vars <- c("sr", "logv", "ele", "slope", "tem", "prec", "ndvi", "hf", "lon", "lat")
 com_clean <- com[complete.cases(com[, vars]), ]
-#1002623
-
-
 data_in <- com_clean %>% filter(in_PA == 'Inside PA')
+
+data_in$logsr <- log(data_in$sr)
+data_in$land <- as.factor(data_in$land)
+pos <- data_in$visit_intensity[!is.na(data_in$visit_intensity) & data_in$visit_intensity > 0]
+c0 <- 0.5 * min(pos)
+data_in$logv <- log(data_in$visit_intensity + c0)
 
 fit <- gam(
   logsr ~ s(logv, k = 3, bs = "ts") +
@@ -237,30 +214,25 @@ DHARMa::testDispersion(sim_gam)
 
 library(visreg)
 v <- visreg(fit, "logv", scale = "response", partial = TRUE)
-fit_df <- v$fit
-partial_df <- v$res
-fit_df$x <- fit_df$logv  
+fit_df <- v$fit %>%
+  mutate(
+    # Use a small threshold to force the tiny numbers to 0
+    visit_raw = exp(logv) - c0,
+    
+    sr_fit    = exp(visregFit),
+    sr_lwr    = exp(visregLwr),
+    sr_upr    = exp(visregUpr)
+  )
 
-ggplot() +
-  #geom_point(data = partial_df, aes(x = logv, y = visregRes), color = "gray50", alpha = 0.1, size = 0.5) +  # partial residuals
-  geom_ribbon(data = fit_df, aes(x = x, ymin = visregLwr, ymax = visregUpr), fill = "#C8E6C9", alpha = 0.6) +  # light green
-  geom_line(data = fit_df, aes(x = x, y = visregFit), color = "#4CAF50", size = 0.6) +  # green line
-  labs(
-    x = "Log(Visit Intensity)",
-    y = "Predicted Species Richness"
-  ) +
+
+ggplot(fit_df, aes(x = logv, y = sr_fit)) +
+  geom_ribbon(aes(ymin = sr_lwr, ymax = sr_upr), fill = "#C8E6C9", alpha = 0.6) +
+  geom_line(color = "#4CAF50", linewidth = 0.8) +
   theme_bw(base_size = 14) +
   theme(
     panel.grid.minor = element_blank(),
     panel.grid.major = element_blank()
   )
-
-library(ggeffects)
-pred <- ggpredict(fit, terms = "logv")
-turning_point <- pred$x[which.max(pred$predicted)]
-exp(turning_point) - 0.1
-#3.659356
-#38.73632
 
 
 
